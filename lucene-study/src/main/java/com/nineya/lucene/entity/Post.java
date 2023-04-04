ac281fc6b7b5643197892603ec95b3ebbd39e0ce
nineya <361654768@qq.com>
2023-04-04 09:08:59 +0800

    chore(lucene): lucene study

--- lucene-study/src/main/java/com/nineya/lucene/entity/Post.java
+++ lucene-study/src/main/java/com/nineya/lucene/entity/Post.java
@@ -1,17 +1,1 @@
-package com.nineya.lucene.entity;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class Post {
-    private Long id;
-    private String title;
-    private String categories;
-    // 标签
-    private String[] tags;
-    private String content;
-}
+package com.nineya.lucene.entity;import lombok.AllArgsConstructor;import lombok.Data;import lombok.NoArgsConstructor;@Data@NoArgsConstructor@AllArgsConstructorpublic class Post {    private Long id;    private String title;    private String categories;    // 标签    private String[] tags;    private String content;}
