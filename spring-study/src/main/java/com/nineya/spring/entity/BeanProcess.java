5a4d5941935b463cda30788cda88efcd2b72dc74
nineya <361654768@qq.com>
2023-08-04 17:35:26 +0800

    feat(spring): 添加生命周期注解

--- spring-study/src/main/java/com/nineya/spring/entity/BeanProcess.java
+++ spring-study/src/main/java/com/nineya/spring/entity/BeanProcess.java
@@ -3,15 +3,20 @@ package com.nineya.spring.entity;
 import lombok.Data;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Data
 @Component
 public class BeanProcess {
     private String name;
 
+    @PostConstruct
     public void init() {
         System.out.println("Bean InitMethodName");
     }
 
+    @PreDestroy
     public void destroy() {
         System.out.println("Bean DestroyMethodName");
     }
