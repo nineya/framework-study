dbb7f82f26e1cfbc00e3d52fb14b6d261f4399f3
nineya <361654768@qq.com>
2023-08-04 17:33:34 +0800

    feat(springboot): 添加springboot

---
+++ spring-study/src/main/java/com/nineya/spring/processor/NineyaBeanFactoryPostProcessor.java
@@ -0,0 +1,15 @@
+package com.nineya.spring.processor;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NineyaBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+        System.out.println("BeanFactoryPostProcessor.postProcessBeanFactory 后置处理器");
+    }
+}
---
+++ spring-study/src/main/java/com/nineya/spring/processor/NineyaBeanPostProcessor.java
@@ -0,0 +1,20 @@
+package com.nineya.spring.processor;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NineyaBeanPostProcessor implements BeanPostProcessor {
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("BeanPostProcessor.postProcessAfterInitialization(" + beanName + ") 后置处理器");
+        return bean;
+    }
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        System.out.println("BeanPostProcessor.postProcessBeforeInitialization(" + beanName + ") 后置处理器");
+        return bean;
+    }
+}
